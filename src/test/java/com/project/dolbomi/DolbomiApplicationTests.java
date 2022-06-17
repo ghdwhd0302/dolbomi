bb81f95998a2ee59961203c33bd30db67a3efacc
dongsun7235 <dongsun7235@gmail.com>
2022-06-18 02:11:25 +0900

    git

---
+++ src/test/java/com/project/dolbomi/DolbomiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.dolbomi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DolbomiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
