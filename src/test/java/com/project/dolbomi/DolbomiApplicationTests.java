9779ed1cd9f2c6f42b3388309582155b0850b0ba
da2hn <apple7278@naver.com>
2022-06-20 00:41:21 +0900

    pom

--- src/test/java/com/project/dolbomi/DolbomiApplicationTests.java
+++ src/test/java/com/project/dolbomi/DolbomiApplicationTests.java
@@ -6,14 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DolbomiApplicationTests {
 
-<<<<<<< HEAD
-	@Test
-	void contextLoads() {
-	}
-=======
     @Test
     void contextLoads() {
     }
->>>>>>> 624f493c4f168018675c58e53ec22e3f22de258e
 
 }
