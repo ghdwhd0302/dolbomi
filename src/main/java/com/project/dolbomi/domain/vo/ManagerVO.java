3fdfee0fcbce4c8e278935b8b3198ba50d9f623e
yunbo3 <yunbo3@naver.com>
2022-06-20 09:07:44 +0900

    VO 수정

---
+++ src/main/java/com/project/dolbomi/domain/vo/ManagerVO.java
@@ -0,0 +1,19 @@
+package com.project.dolbomi.domain.vo;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+public class ManagerVO {
+    private String managerName;
+    private String managerBirth;
+    private String managerEmail;
+    private String managerGender;
+    private String managerDriveOk;
+    private String managerPhoneNum;
+    private String managerAddress;
+    private String managerCareer;
+    private String managerPw;
+}
+
---
+++ src/main/java/com/project/dolbomi/domain/vo/ReviewVO.java
@@ -0,0 +1,17 @@
+package com.project.dolbomi.domain.vo;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+public class ReviewVO {
+    private Long reviewNum;
+    private String reviewType;
+    private String reviewTitle;
+    private String reviewContent;
+    private String reviewWriter;
+    private String reviewScore;
+    private String reviewDate;
+    private String reviewUpdateDate;
+}
