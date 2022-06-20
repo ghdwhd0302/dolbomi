2ca4cfeddd56ac79daaa1be4fd4af27889169aa3
yunbo3 <yunbo3@naver.com>
2022-06-20 09:12:30 +0900

    수정

--- src/main/java/com/project/dolbomi/DolbomiApplication.java
+++ src/main/java/com/project/dolbomi/DolbomiApplication.java
@@ -6,14 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DolbomiApplication {
 
-<<<<<<< HEAD
-	public static void main(String[] args) {
-		SpringApplication.run(DolbomiApplication.class, args);
-	}
-=======
+
     public static void main(String[] args) {
         SpringApplication.run(DolbomiApplication.class, args);
     }
->>>>>>> 624f493c4f168018675c58e53ec22e3f22de258e
+
 
 }
