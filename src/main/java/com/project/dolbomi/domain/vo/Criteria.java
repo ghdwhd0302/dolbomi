3a2fe36792222c50c9baf9d935f4c4b6105f76fd
yunbo3 <yunbo3@naver.com>
2022-06-22 18:21:28 +0900

    페이징처리

---
+++ src/main/java/com/project/dolbomi/domain/vo/Criteria.java
@@ -0,0 +1,17 @@
+package com.project.dolbomi.domain.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+@AllArgsConstructor
+public class Criteria {
+    private Integer pageNum;
+    private Integer amount;
+
+    public Criteria() {
+        this(1, 10);
+    }
+}
