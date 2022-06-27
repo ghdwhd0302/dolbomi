51372b17562b89e17a5369b6efc59c51961cca42
yunbo3 <yunbo3@naver.com>
2022-06-27 17:11:57 +0900

    테스트

--- src/test/java/com/project/dolbomi/doamin/dao/CareReservationDaoTests.java
+++ src/test/java/com/project/dolbomi/doamin/dao/CareReservationDaoTests.java
@@ -1,9 +1,7 @@
 package com.project.dolbomi.doamin.dao;
 
 import com.project.dolbomi.domain.dao.CareReservationDAO;
-import com.project.dolbomi.domain.vo.CareReservationVO;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
--- src/test/java/com/project/dolbomi/mapper/UserMapperTests.java
+++ src/test/java/com/project/dolbomi/mapper/UserMapperTests.java
@@ -1,6 +1,8 @@
 package com.project.dolbomi.mapper;
 
+import com.project.dolbomi.domain.vo.UserVO;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,6 +11,44 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class UserMapperTests {
     @Autowired
     private UserMapper userMapper;
+    private UserVO userVO;
 
+
+
+    /*@Test
+    public void updateTest(){
+        UserVO userVO=userMapper.selectProfile("APPLE");
+
+        userVO.setUserName("강남");
+        userVO.setUserBirth("19991222");
+        userVO.setUserPw("asdasd");
+
+    }*/
+    /*@Test
+    public void updateTest(){
+        UserVO userVO=userMapper.selectProfile("APPLE");
+        log.info(String.valueOf(userVO));
+        userVO.setUserName("강남동");
+        userVO.setUserBirth("19991222");
+        userVO.setUserPw("asdasd");
+        log.info(String.valueOf(userVO));
+    }*/
+   /* @Test
+    public void deleteTest(){
+
+
+        log.info(userMapper.delete("APPLE"));
+
+    }*/
+    /*@Test
+    public void getUserTest(){
+        log.info(userMapper.selectProfile("APPLE"));
+    }*/
+
+    @Test
+    public void getUserTest(){
+
+        log.info(userMapper.selectProfile("APPLE").toString());
+    }
 
 }
