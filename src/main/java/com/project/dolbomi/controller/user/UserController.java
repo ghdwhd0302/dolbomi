0491846f9c7ebef81ace30ab48177387db7cef74
dongsun7235 <dongsun7235@gmail.com>
2022-06-30 02:49:47 +0900

    수정

--- src/main/java/com/project/dolbomi/controller/user/UserController.java
+++ src/main/java/com/project/dolbomi/controller/user/UserController.java
@@ -59,10 +59,6 @@ public class UserController {
     }
 
 //    휴대폰 인증번호 발송
-//    @GetMapping("sendcode")
-//    public void phoneCerti(){
-//
-//    }
 
     @PostMapping("sendcode")
     public String phoneCerti(){
@@ -73,14 +69,8 @@ public class UserController {
     @ResponseBody
     public String sendSMS(@RequestParam("phone") String userPhoneNum) { // 휴대폰 문자보내기
         int randomNum = (int)((Math.random()* (9999 - 1000 + 1)) + 1000);//난수 생성
-
-<<<<<<< HEAD
-        userService.phoneCerti(userPhoneNum, randomNum);
-
-=======
         userService.phoneCerti(userPhoneNum,randomNum);
         log.info(Integer.toString(randomNum));
->>>>>>> 53c848502eaa023b63daadfe96bcd295fd83c516
         return Integer.toString(randomNum);
     }
 
